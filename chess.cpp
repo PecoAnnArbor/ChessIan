be4ebaab8fbace25c012b3e0332d15476d682ee7
Ian Zhang <ianzhang3374@gmail.com>
2016-11-15 18:12:17 -0500

    modified it

--- chess.cpp
+++ chess.cpp
@@ -34,35 +34,64 @@ void printBoard(char board[8][8]) {
     }
 }
 
-bool isValidMove(int inCol, int inRow, int outCol, int outRow, char board[8][8]) {
+bool isValidMoveRook(int inCol, int inRow, int outCol, int outRow, char board[8][8]) {
     int smallCol = min(inCol, outCol);
     int smallRow = min(inRow, outRow);
     int maxCol = max(inCol, outCol);
     int maxRow = max(inRow, outRow);
-    if(board[inRow][inCol] == '-') {
-        return false;
-    }
-    else {
-        if(board[inRow][inCol] == 'R' || board[inRow][inCol] == 'r') {
-            if(inRow == outRow) {
-                for(int i = smallCol + 1; i < maxCol; i++) {
-                    if(board[inRow][i] != '-') {
-                        return false;
-                    }
-                }
-            }
-            else if(outCol == inCol) {
-                for(int i = smallRow + 1; i < maxRow; i++) {
-                    if(board[i][inCol] != '-') {
-                        return false;
-                    }
-                }
-            }
-            else {
+    if(inRow == outRow) {
+        for(int i = smallCol + 1; i < maxCol; i++) {
+            if(board[inRow][i] != '-') {
                 return false;
             }
         }
         return true;
+    }
+    else if(outCol == inCol) {
+        for(int i = smallRow + 1; i < maxRow; i++) {
+            if(board[i][inCol] != '-') {
+                return false;
+            }
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+bool isValidMoveBishop(int inCol, int inRow, int outCol, int outRow, char board[8][8]) {
+    int rowDiff = outRow - inRow;
+    int colDiff = outCol - inCol;
+    if(abs(rowDiff) != abs(colDiff)) {
+        return false;
+    }
+    else {
+        for (int i = 0; i < count; ++i){
+            /* code */
+        }
+        for (int i = 0; i < count; ++i){
+            /* code */
+        }
+        for (int i = 0; i < count; ++i) {
+            /* code */
+        }
+        for (int i = 0; i < count; ++i){
+            /* code */
+        }
+
+
+        return true;
+    }
+}
+
+bool isValidMove(int inCol, int inRow, int outCol, int outRow, char board[8][8]) {
+    if(board[inRow][inCol] == '-') {
+        return false;
+    }
+    else {
+        if((board[inRow][inCol] == 'R' || board[inRow][inCol] == 'r') && isValidMoveRook(inCol, inRow, outCol, outRow, board)) {
+        return true;
+        }   
     }
 }
 
@@ -107,6 +136,6 @@ int main() {
         movePiece(board);
         printBoard(board);
     }
-    return 0;
 } 
+
 
